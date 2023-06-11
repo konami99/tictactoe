678a5f5c3a9fa2a3f2b57e19adaef0ec1f0d9f79
Richard Chou <richard@ezyraise.com>
2023-06-12 09:49:32 +1000

    defined winner

--- app/models/tictactoeboard.rb
+++ app/models/tictactoeboard.rb
@@ -1,5 +1,27 @@
 class Tictactoeboard < ApplicationRecord
-
   after_update_commit -> { broadcast_replace_to "tictoctoeboards" }
 
+  def winner
+    if cell_1.present? && cell_2.present? && cell_3.present? && cell_1 == cell_2 && cell_2 == cell_3
+      cell_1
+    elsif cell_4.present? && cell_5.present? && cell_6.present? && cell_4 == cell_5 && cell_5 == cell_6
+      cell_4
+    elsif cell_7.present? && cell_8.present? && cell_9.present? && cell_7 == cell_8 && cell_8 == cell_9
+      cell_7
+    elsif cell_1.present? && cell_4.present? && cell_7.present? && cell_1 == cell_4 && cell_4 == cell_7
+      cell_1
+    elsif cell_2.present? && cell_5.present? && cell_8.present? && cell_2 == cell_5 && cell_5 == cell_8
+      cell_2
+    elsif cell_3.present? && cell_6.present? && cell_9.present? && cell_3 == cell_6 && cell_6 == cell_9
+      cell_3
+    elsif cell_1.present? && cell_5.present? && cell_9.present? && cell_1 == cell_5 && cell_5 == cell_9
+      cell_1
+    elsif cell_3.present? && cell_5.present? && cell_7.present? && cell_3 == cell_5 && cell_5 == cell_7
+      cell_3
+    end
+  end
+
+  def whose_turn
+
+  end
 end
