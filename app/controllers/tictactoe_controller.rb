9afe61de8708a3d8cbf34ede13aca1de47693178
Richard Chou <richard@ezyraise.com>
2023-06-11 18:09:35 +1000

    catch click action

--- app/controllers/tictactoe_controller.rb
+++ app/controllers/tictactoe_controller.rb
@@ -2,4 +2,12 @@ class TictactoeController < ApplicationController
   def index
 
   end
+
+  def click
+    
+    respond_to do |format|
+      format.html {  }
+      format.turbo_stream
+    end
+  end
 end
---
+++ app/javascript/controllers/tictactoe_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    console.log(this.element);
+  }
+
+  click(e) {
+    console.log('click');
+  }
+}
